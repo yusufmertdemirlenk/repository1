8f418972411990aa068d5defbef6abb5657489ac
yusufmertdemirlenk <42177768+yusufmertdemirlenk@users.noreply.github.com>
2018-08-07 09:56:58 -0400

    adding lines for mean and std

--- hist.R
+++ hist.R
@@ -4,3 +4,6 @@ xbar <- mean(x)
 s <- sd(x)
 
 hist(smpl)
+abline(v = xbar, lwd = 2, col = "red")
+abline(v = xbar + 2*s, lwd = 2, col = "red", lty = 3)
+abline(v = xbar - 2*s, lwd = 2, col = "red", lty = 3)
